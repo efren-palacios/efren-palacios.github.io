264bcab2ac01b8004f925edce97461d9c347e2ce
orctamer <efrenpalacios2011@live.com>
2019-01-27 21:29:52 -0800

    fixed hamburger fixed

--- echofox/js/app.js
+++ echofox/js/app.js
@@ -5,7 +5,9 @@ window.onscroll = function() {
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     document.getElementById("nav").style.height = "53px";
+    document.getElementById("hamburger").style.top = "20px";
   } else {
     document.getElementById("nav").style.height = "93px";
+    document.getElementById("hamburger").style.top = "40px";
   }
 }
