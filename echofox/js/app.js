2ed36dd179963e7bed30d1ba64e06b1e851ae049
orctamer <efrenpalacios2011@live.com>
2019-01-29 10:30:02 -0800

    added gallery and zoom

--- echofox/js/app.js
+++ echofox/js/app.js
@@ -11,3 +11,38 @@ function scrollFunction() {
     document.getElementById("hamburger").style.top = "40px";
   }
 }
+
+window.onload = function() {
+  let image = document.getElementById("main-img");
+  let thumbs = document.getElementsByClassName("image-thumb");
+  for (var i = 0; i < thumbs.length; i++) {
+    thumbs[i].addEventListener("click", function() {
+      let img = `url('${this.src}')`;
+      image.style.backgroundImage = img;
+    });
+  }
+  // Init Gallery Zoom //
+  image.addEventListener(
+    "mouseenter",
+    function(e) {
+      this.style.backgroundSize = "250%";
+    },
+    false
+  );
+  image.addEventListener(
+    "mousemove",
+    function(e) {
+      var dimentions = this.getBoundingClientRect();
+      var x = e.clientX - dimentions.left;
+      var y = e.clientY - dimentions.top;
+      var xpercent = Math.round(100 / (dimentions.width / x));
+      var ypercent = Math.round(100 / (dimentions.height / y));
+      this.style.backgroundPosition = xpercent + "% " + ypercent + "%";
+    },
+    false
+  );
+  image.addEventListener("mouseleave", function(e) {
+    this.style.backgroundSize = "cover";
+    this.style.backgroundPosition = "center";
+  });
+};
