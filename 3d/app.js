f197cb277d2fc28a9e705e4f79be45712c18b523
orctamer <efrenpalacios2011@live.com>
2020-03-13 02:52:42 -0700

    3d stuff

--- 3d/app.js
+++ 3d/app.js
@@ -1,3 +1,17 @@
 let app = new PIXI.Application({width: window.innerWidth, height: window.innerHeight});
-console.log(app.view);
 document.body.appendChild(app.view);
+let img = new PIXI.Sprite.from('./jotaro.jpg');
+img.width = window.innerWidth;
+app.stage.addChild(img);
+
+let depthMap = new PIXI.Sprite.from('./map.png');
+app.stage.addChild(depthMap);
+
+let displacementFilter = new PIXI.filters.DisplacementFilter(depthMap);
+
+app.stage.filters = [displacementFilter];
+
+window.onmousemove = function (e) {
+  displacementFilter.scale.x = (window.innerWidth / 2 - e.clientX) / 30;
+  displacementFilter.scale.y = (window.innerHeight / 2 - e.clientY) / 30;
+}
