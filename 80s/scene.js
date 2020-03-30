0d6af35153e174111d78c129585071d56f9d7bdf
orctamer <efrenpalacios2011@live.com>
2020-03-30 16:23:00 -0700

    added sun

--- 80s/scene.js
+++ 80s/scene.js
@@ -12,13 +12,41 @@ class Scene {
     this.camera.lookAt(this.scene.position);
     this.scene.add(this.camera);
     this.grid = new THREE.InfiniteGridHelper(50, 50);
+    this.grid.position.z = 500;
     this.scene.add(this.grid);
     this.scene.background = new THREE.Color(0x392759);
     this.animate();
+    // SUN
+    this.sunGeom = new THREE.CircleBufferGeometry(200, 64);
+    this.sunMat = new THREE.MeshBasicMaterial({ color: 0xff8800, fog: false, transparent: true });
+    this.sunMat.onBeforeCompile = shader => {
+      shader.uniforms.time = { value: 0 };
+      shader.vertexShader =
+        `
+    varying vec2 vUv;
+  ` + shader.vertexShader;
+      shader.vertexShader = shader.vertexShader.replace(
+        `#include <begin_vertex>`,
+        `#include <begin_vertex>
+      vUv = uv;
+    `
+      );
+      shader.fragmentShader = `
+    varying vec2 vUv;
+  ` + shader.fragmentShader;
+      shader.fragmentShader = shader.fragmentShader.replace(
+        `gl_FragColor = vec4( outgoingLight, diffuseColor.a );`,
+        `gl_FragColor = vec4( outgoingLight, diffuseColor.a * smoothstep(0.5, 0.75, vUv.y));`
+      );
+    }
+    this.sun = new THREE.Mesh(this.sunGeom, this.sunMat);
+    this.sun.position.set(0, 0, -550);
+    this.scene.add(this.sun);
+
   }
   animate() {
     requestAnimationFrame(() => this.animate());
-    this.camera.position.z += -.25;
+    this.grid.position.z += .25;
     this.renderer.render(this.scene, this.camera);
   }
 }
