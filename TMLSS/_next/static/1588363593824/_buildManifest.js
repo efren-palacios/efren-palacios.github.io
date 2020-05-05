a54a48e6f1e5755897d65cd9ced694b99a72466e
orctamer <efrenpalacios2011@live.com>
2020-05-05 08:43:02 -0700

    removed TMLSS

--- TMLSS/_next/static/1588363593824/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a,b){return {"/":[a,b],"/about":[a,b],"/index":[a,b],"/products/black":[a,b],"/products/white":[a,b]}}("static\u002Fchunks\u002F05d954cf.8e4003c15e09e324ab6a.js","static\u002Fchunks\u002F178ff9b804ef76e202ddbc5bb759d9847f114d90.2571b26817bd6bd65fa7.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- TMLSS/_next/static/1588363758044/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a,b){return {"/":[a,b],"/about":[a,b],"/index":[a,b],"/products/black":[a,b],"/products/white":[a,b]}}("static\u002Fchunks\u002F05d954cf.8e4003c15e09e324ab6a.js","static\u002Fchunks\u002F19efe34f97cd894a7df9a0bf736fb4b0aa1411d0.2571b26817bd6bd65fa7.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
